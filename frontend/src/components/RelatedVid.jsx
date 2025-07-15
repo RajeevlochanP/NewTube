f46617e37143757886267e39d2b24e9a15fda158
unknown <rajeevlochanperla@gmail.com>
2025-07-15 18:09:18 +0530

    component splitting done

---
+++ frontend/src/components/RelatedVid.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from '../styles/Player.module.css'
+
+function RelatedVid({video,index}) {
+    return (
+        <div
+            className={styles.relatedVideo}
+            data-aos="fade-left"
+            data-aos-delay={index * 100}
+        >
+            <div className={styles.relatedThumbnail}>
+                <img src={video.thumbnail} alt={video.title} />
+                <span className={styles.relatedDuration}>{video.duration}</span>
+            </div>
+            <div className={styles.relatedInfo}>
+                <h4 className={styles.relatedTitle}>{video.title}</h4>
+                <p className={styles.relatedChannel}>{video.channel}</p>
+                <div className={styles.relatedMeta}>
+                    <span>{video.views}</span>
+                    <span className={styles.separator}>•</span>
+                    <span>{video.timestamp}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RelatedVid
