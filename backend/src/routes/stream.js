c5e4d416d1a18c56c4dc6da3b5432f1f0b146f35
unknown <rajeevlochanperla@gmail.com>
2025-07-16 19:00:06 +0530

    might have errors

---
+++ backend/src/routes/stream.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { sendVideo, toggleLike } from "../controllers/stream.controller.js"
+import { checkToken, requireUser } from "../middlewares/protect.js";
+import { addComment,deleteComment } from "../controllers/stream.controller.js";
+
+const router = express.Router();
+
+router.get("/:videoId", sendVideo);
+
+router.post("addComment/:videoId",checkToken,requireUser,addComment);
+router.post("deleteComment/:commentId",checkToken,requireUser,deleteComment);
+
+router.post("/toggleLike/:videoId",checkToken,requireUser,toggleLike);
